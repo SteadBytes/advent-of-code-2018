7087a1e89ae7424b63f38c137d36d24b56a2965b
Ben Steadman <steadmanben1@gmail.com>
2018-12-08 08:44:16 +0000

    day 8 extract parsing

--- 08/solution.py
+++ 08/solution.py
@@ -1,3 +1,41 @@
+def parse_entries(entries):
+    """
+    Returns:
+        (int, int): sum of all metadata entries, root node value - see puzzle
+            description
+    """
+
+    def recurse(entries):
+        n_child, n_meta = entries[:2]
+        remaining = entries[2:]
+
+        meta_total = 0
+        child_node_values = []
+
+        for _ in range(n_child):
+            child_total, child_value, remaining = recurse(remaining)
+            meta_total += child_total
+            child_node_values.append(child_value)
+
+        current_node_meta = remaining[:n_meta]
+        current_node_meta_total = sum(current_node_meta)
+
+        meta_total += current_node_meta_total
+
+        if n_child == 0:
+            current_node_value = current_node_meta_total
+        else:
+            current_node_value = sum(
+                child_node_values[i - 1]  # account for 0 based index
+                for i in current_node_meta
+                if i > 0 and i <= len(child_node_values)
+            )
+
+        return meta_total, current_node_value, remaining[n_meta:]
+
+    return recurse(entries)[:2]
+
+
 def part_1(entries):
     """
     Basic premise:
@@ -13,55 +51,11 @@ def part_1(entries):
     5. return meta_total, remaining_entries
     """
 
-    def sum_metadata(entries):
-        n_child, n_meta = entries[:2]
-        remaining_entries = entries[2:]
-
-        meta_total = 0
-
-        for _ in range(n_child):
-            child_total, remaining_entries = sum_metadata(remaining_entries)
-            meta_total += child_total
-
-        meta_total += sum(remaining_entries[:n_meta])
-
-        return meta_total, remaining_entries[n_meta:]
-
-    return sum_metadata(entries)[0]
+    return parse_entries(entries)[0]
 
 
 def part_2(entries):
-    def sum_metadata(entries):
-        n_child, n_meta = entries[:2]
-        remaining_entries = entries[2:]
-
-        meta_total = 0
-        child_node_values = []
-
-        for _ in range(n_child):
-            child_total, child_value, remaining_entries = sum_metadata(
-                remaining_entries
-            )
-            meta_total += child_total
-            child_node_values.append(child_value)
-
-        current_node_meta = remaining_entries[:n_meta]
-        current_node_meta_total = sum(current_node_meta)
-
-        meta_total += current_node_meta_total
-
-        if n_child == 0:
-            current_node_value = current_node_meta_total
-        else:
-            current_node_value = sum(
-                child_node_values[i - 1]  # account for 0 based index
-                for i in current_node_meta
-                if i > 0 and i <= len(child_node_values)
-            )
-
-        return meta_total, current_node_value, remaining_entries[n_meta:]
-
-    return sum_metadata(entries)[1]
+    return parse_entries(entries)[1]
 
 
 def main(puzzle_input_f):
