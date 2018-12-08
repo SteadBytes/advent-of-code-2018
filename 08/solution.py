f77bfff888f1b023d8442cf2451a546e0513909b
Ben Steadman <steadmanben1@gmail.com>
2018-12-08 08:39:33 +0000

    day 8 p2 initial

--- 08/solution.py
+++ 08/solution.py
@@ -31,7 +31,37 @@ def part_1(entries):
 
 
 def part_2(entries):
-    pass
+    def sum_metadata(entries):
+        n_child, n_meta = entries[:2]
+        remaining_entries = entries[2:]
+
+        meta_total = 0
+        child_node_values = []
+
+        for _ in range(n_child):
+            child_total, child_value, remaining_entries = sum_metadata(
+                remaining_entries
+            )
+            meta_total += child_total
+            child_node_values.append(child_value)
+
+        current_node_meta = remaining_entries[:n_meta]
+        current_node_meta_total = sum(current_node_meta)
+
+        meta_total += current_node_meta_total
+
+        if n_child == 0:
+            current_node_value = current_node_meta_total
+        else:
+            current_node_value = sum(
+                child_node_values[i - 1]  # account for 0 based index
+                for i in current_node_meta
+                if i > 0 and i <= len(child_node_values)
+            )
+
+        return meta_total, current_node_value, remaining_entries[n_meta:]
+
+    return sum_metadata(entries)[1]
 
 
 def main(puzzle_input_f):
