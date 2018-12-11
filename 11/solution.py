f5a23841932e1119ebeec3240cb90a8a7c88902a
Ben Steadman <steadmanben1@gmail.com>
2018-12-11 21:09:33 +0000

    day 11 extract partial sums calculation

--- 11/solution.py
+++ 11/solution.py
@@ -9,7 +9,7 @@ def cell_power(x, y, serial_number):
     return hundreds - 5
 
 
-def part_1(serial_number):
+def build_partial_sums(serial_number):
     partial_sums = defaultdict(int)
 
     def ps(x, y):
@@ -23,7 +23,10 @@ def part_1(serial_number):
     for y in range(300):
         for x in range(300):
             partial_sums[x, y] = ps(x, y)
+    return partial_sums
 
+
+def part_1(partial_sums):
     width = height = 3
     grid_sums = {}
     for y in range(300):
@@ -40,26 +43,12 @@ def part_1(serial_number):
     return max_pos[0] - 2, max_pos[1] - 2
 
 
-def part_2(serial_number):
-    partial_sums = defaultdict(int)
-
-    def ps(x, y):
-        return (
-            cell_power(x, y, serial_number)
-            + partial_sums[x, y - 1]
-            + partial_sums[x - 1, y]
-            - partial_sums[x - 1, y - 1]
-        )
-
-    for y in range(300):
-        for x in range(300):
-            partial_sums[x, y] = ps(x, y)
-
+def part_2(partial_sums):
     grid_sums = {}
     for s in range(2, 300):
-        for y in range(s-1, 300):
-            for x in range(s-1, 300):
-                grid_sums[x-s+1, y-s+1, s] = (
+        for y in range(s - 1, 300):
+            for x in range(s - 1, 300):
+                grid_sums[x - s + 1, y - s + 1, s] = (
                     partial_sums[x, y]
                     + partial_sums[x - s, y - s]
                     - partial_sums[x - s, y]
@@ -68,14 +57,15 @@ def part_2(serial_number):
 
     max_pos = max(grid_sums, key=grid_sums.get)
     max_sum = grid_sums[max_pos]
-    return max_pos[0] , max_pos[1] , max_pos[2]
+    return max_pos[0], max_pos[1], max_pos[2]
 
 
 def main(puzzle_input_f):
     serial_number = int(f.read())
+    partial_sums = build_partial_sums(serial_number)
 
-    print("Part 1: ", part_1(serial_number))
-    print("Part 2: ", part_2(serial_number))
+    print("Part 1: ", part_1(partial_sums))
+    print("Part 2: ", part_2(partial_sums))
 
 
 if __name__ == "__main__":
