3ed45f93f3d46ba6cc9b87d6ae4dd1a6e5d5b7f7
Ben Steadman <steadmanben1@gmail.com>
2018-12-15 12:09:10 +0000

    day 14 p2 initial

--- 14/solution.py
+++ 14/solution.py
@@ -16,14 +16,34 @@ def part_1(num_recipes):
 
 
 def part_2(num_recipes):
-    pass
+    scoreboard = [3, 7]
+    elves = [0, 1]
+
+    score_sequence = [int(s) for s in str(num_recipes)]
+
+    def continue_search():
+        return (
+            scoreboard[-len(score_sequence) :] != score_sequence
+            and scoreboard[-len(score_sequence) - 1 : -1] != score_sequence
+        )
+
+    while continue_search():
+        recipe_total = sum(scoreboard[p] for p in elves)
+        if recipe_total < 10:
+            scoreboard.append(recipe_total)
+        else:
+            new_scores = divmod(recipe_total, 10)
+            scoreboard.extend(new_scores)
+
+        elves = [(p + 1 + scoreboard[p]) % len(scoreboard) for p in elves]
+    return len(scoreboard) - len(score_sequence) - 1
 
 
 def main(puzzle_input_f):
     num_recipes = int(puzzle_input_f.readline())
 
     print("Part 1: ", part_1(num_recipes))
-    print("Part 2: ", part_2())
+    print("Part 2: ", part_2(num_recipes))
 
 
 if __name__ == "__main__":
