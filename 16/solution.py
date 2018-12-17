2fc43160c1d8ad80f5f25c926c40d7c2a0dc5647
Ben Steadman <steadmanben1@gmail.com>
2018-12-17 21:47:27 +0000

    day 16 p2

--- 16/solution.py
+++ 16/solution.py
@@ -1,68 +1,71 @@
 import re
+from collections import defaultdict, namedtuple
 
 
 def addr(A, B, before):
+    """ Reg C -> result of adding reg A and reg B
+    """
     return before[A] + before[B]
 
 
 def addi(A, B, before):
+    """ Reg C -> result of adding reg A and val B
+    """
     return before[A] + B
 
 
 def mulr(A, B, before):
+    """ Reg C -> result of multiplying reg A and reg B
+    """
     return before[A] * before[B]
 
 
 def muli(A, B, before):
+    """ Reg C -> result of multiplying reg A and val B
+    """
     return before[A] * B
 
 
 def banr(A, B, before):
+    """ Reg C -> result of bitwise AND reg A and reg B
+    """
     return before[A] & before[B]
 
 
 def bani(A, B, before):
+    """ Reg C -> result of bitwise AND reg A and val B
+    """
     return before[A] & B
 
 
 def borr(A, B, before):
+    """ Reg C -> result of bitwise OR reg A and reg B
+    """
     return before[A] | before[B]
 
 
 def bori(A, B, before):
+    """ Reg C -> result of bitwose OR reg A and val B
+    """
     return before[A] | B
 
 
 def setr(A, B, before):
+    """ Reg C -> contents of reg A (input B ignored)
+    """
     return before[A]
 
 
 def seti(A, B, before):
+    """ Reg C -> val A (input B ignored)
+    """
     return A
 
 
 def gtir(A, B, before):
+    """ Reg C -> 1 if val A > reg B else 0
+    """
     return int(A > before[B])
-
-
-def gtri(A, B, before):
-    return int(before[A] > B)
-
-
-def gtrr(A, B, before):
-    return int(before[A] > before[B])
-
-
-def eqir(A, B, before):
-    return int(A == before[B])
-
-
-def eqri(A, B, before):
-    return int(before[A] == B)
-
-
-def eqrr(A, B, before):
-    return int(before[A] == before[B])
 
 
 opcodes = [
@@ -103,22 +106,46 @@ def parse_samples_input(samples_input):
     return samples
 
 
+def parse_program_input(program_input):
+    lines = (l for l in program_input.split("\n") if l)
+    return [[int(x) for x in l.strip().split()] for l in lines]
+
+
 def part_1(samples):
     possible_ops = [possible_opcodes(s) for s in samples]
 
     return len([s for s in possible_ops if len(s) >= 3])
 
 
-def part_2():
-    pass
+def part_2(samples, program):
+    possible_ops = defaultdict(lambda: set(opcodes))
+    for s in samples:
+        op_num = s.inst[0]
+        possible_ops[op_num] &= set(possible_opcodes(s))
+
+    op_num_map = {}
+    while any(possible_ops.values()):
+        known = [op_num for op_num, ops in possible_ops.items() if len(ops) == 1]
+
+        for op_num in known:
+            op_num_map[op_num] = op = possible_ops[op_num].pop()
+            for possible in possible_ops.values():
+                possible.discard(op)
+
+    registers = [0] * 4
+    for op_num, A, B, C in program:
+        registers[C] = op_num_map[op_num](A, B, registers)
+
+    return registers[0]
 
 
 def main(puzzle_input_f):
     samples_input, program_input = puzzle_input_f.read().split("\n\n\n")
     samples = parse_samples_input(samples_input)
+    program = parse_program_input(program_input)
 
     print("Part 1: ", part_1(samples))
-    print("Part 2: ", part_2())
+    print("Part 2: ", part_2(samples, program))
 
 
 if __name__ == "__main__":
