8c973963b2ee9238a5875c00620e34df201ba4ff
Ben Steadman <steadmanben1@gmail.com>
2018-12-21 08:18:24 +0000

    day 20 p2 initial

--- 20/solution.py
+++ 20/solution.py
@@ -37,15 +37,34 @@ def part_1(regex):
     return max(m.values())
 
 
-def part_2():
-    pass
+def part_2(regex):
+    m = defaultdict(lambda: float("inf"))
+    current = Node(Point(0, 0), 0)
+    s = [current]
+
+    for ch in regex:
+        if ch in directions:
+            pt = current.pt + directions[ch]
+            dist = min(m[pt], current.dist + 1)
+            node = Node(pt, dist)
+            current = node
+            m[node.pt] = node.dist
+        elif ch == "(":
+            s.append(current)
+            pass
+        elif ch == ")":
+            current = s.pop()
+        elif ch == "|":
+            current = s[-1]
+
+    return len([d for d in m.values() if d >= 1000])
 
 
 def main(puzzle_input_f):
     regex = puzzle_input_f.read().strip()[1:-1]
 
     print("Part 1: ", part_1(regex))
-    print("Part 2: ", part_2())
+    print("Part 2: ", part_2(regex))
 
 
 if __name__ == "__main__":
