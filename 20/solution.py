2dc5ed03d02c6a99ccca0e1ab565e15d4e8d242b
Ben Steadman <steadmanben1@gmail.com>
2018-12-21 08:22:22 +0000

    day 20 refactor

--- 20/solution.py
+++ 20/solution.py
@@ -8,18 +8,21 @@ class Point(namedtuple("Point", ["x", "y"])):
 
 Node = namedtuple("Node", ["pt", "dist"])
 
-directions = {"N": Point(0, -1), "S": Point(0, 1), "E": Point(1, 0), "W": Point(-1, 0)}
 
-
-def part_1(regex):
+def build_facility_map(regex):
     # DFS to build map of rooms -> dists
-    # Return max dist
+    directions = {
+        "N": Point(0, -1),
+        "S": Point(0, 1),
+        "E": Point(1, 0),
+        "W": Point(-1, 0),
+    }
 
     m = defaultdict(lambda: float("inf"))
     current = Node(Point(0, 0), 0)
     s = [current]
 
-    for ch in regex:
+    for ch in regex[1:-1]:  # skip start and end anchors
         if ch in directions:
             pt = current.pt + directions[ch]
             dist = min(m[pt], current.dist + 1)
@@ -33,38 +36,23 @@ def part_1(regex):
             current = s.pop()
         elif ch == "|":
             current = s[-1]
+    return m
 
-    return max(m.values())
+
+def part_1(facility_map):
+    return max(facility_map.values())
 
 
-def part_2(regex):
-    m = defaultdict(lambda: float("inf"))
-    current = Node(Point(0, 0), 0)
-    s = [current]
-
-    for ch in regex:
-        if ch in directions:
-            pt = current.pt + directions[ch]
-            dist = min(m[pt], current.dist + 1)
-            node = Node(pt, dist)
-            current = node
-            m[node.pt] = node.dist
-        elif ch == "(":
-            s.append(current)
-            pass
-        elif ch == ")":
-            current = s.pop()
-        elif ch == "|":
-            current = s[-1]
-
-    return len([d for d in m.values() if d >= 1000])
+def part_2(facility_map):
+    return len([d for d in facility_map.values() if d >= 1000])
 
 
 def main(puzzle_input_f):
-    regex = puzzle_input_f.read().strip()[1:-1]
+    regex = puzzle_input_f.read().strip()
+    facility_map = build_facility_map(regex)
 
-    print("Part 1: ", part_1(regex))
-    print("Part 2: ", part_2(regex))
+    print("Part 1: ", part_1(facility_map))
+    print("Part 2: ", part_2(facility_map))
 
 
 if __name__ == "__main__":
